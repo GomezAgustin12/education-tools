80810edf7ce7579573fdd6f22d4dfcdc0bab5757
benjamin.kugler <benjamin.kugler@elliemae.com>
2020-03-21 13:51:05 -0300

    Responsive home

--- src/Components/Tarjetas/Tarjetas.jsx
+++ src/Components/Tarjetas/Tarjetas.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Card } from "primereact/card";
-import { Button } from "primereact/button";
-import imagen from "./react.png";
-import "./styles/styles.css";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import imagen from './react.png';
+import './styles/styles.css';
+import axios from 'axios';
 
-const url = "https://educacion-remota.herokuapp.com/";
+const url = 'https://educacion-remota.herokuapp.com/';
 
-function Tarjetas() {
+function Tarjetas({ title, subtitle, description }) {
 	const [cards, setCards] = useState([{}]);
 
 	useEffect(() => {
@@ -18,29 +18,24 @@ function Tarjetas() {
 		});
 	}, []);
 
-	const header = <img src={imagen} alt='Card' height='100' width='100' />;
+	const header = <img src={imagen} alt="Card" height="100" width="100" />;
 	const footer = (
 		<span>
-			<Button label='Learn' />
-			<a href='https://drive.google.com/drive/' target='_blank'>
-				<Button label='Site' />
+			<Button label="Learn" />
+			<a href="https://drive.google.com/drive/" target="_blank">
+				<Button label="Site" />
 			</a>
 		</span>
 	);
 	return (
 		<Card
-			title='Advanced Card'
-			subTitle='Subtitle'
-			style={{ width: "360px" }}
-			className='card'
+			title={title}
+			subTitle={subtitle}
+			className="card"
 			footer={footer}
-			header={header}>
-			<div className='contenido'>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-				Inventore sed consequuntur error repudiandae numquam deserunt
-				quisquam repellat libero asperiores earum nam nobis, culpa
-				ratione quam perferendis esse, cupiditate neque quas!
-			</div>
+			header={header}
+		>
+			<div className="contenido">{description}</div>
 		</Card>
 	);
 }
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Tarjetas } from '../../Components';
+import './styles/styles.css';
+
+function Home() {
+	return (
+		<div className="home-container">
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+			<Tarjetas
+				title="Title"
+				subtitle="Subtitle"
+				description="Some escription"
+			/>
+		</div>
+	);
+}
+
+export default Home;
