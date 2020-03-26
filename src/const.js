7abdb9bd7196323da92fbd3d31596c9f1ddc0b97
Agustin Gomez <agustin.gomez@devlights.com>
2020-03-25 22:15:15 -0300

    a

--- src/const.js
+++ src/const.js
@@ -1,3 +1,3 @@
-// export const url = "https://apieducation-tools.404webs.com/";
-export const url = "https://educacion-remota.herokuapp.com/";
+export const url = "https://apieducation-tools.404webs.com/";
+// export const url = "https://educacion-remota.herokuapp.com/";
 // export const url = "http://localhost:4000/";
